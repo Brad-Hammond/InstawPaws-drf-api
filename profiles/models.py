8ac4c40a4e076812b04fa7fbc7c9a12decb55d89
Brad-Hammond <brad_hammond1@sky.com>
2024-12-16 15:27:04 +0000

    Feat: Added profiles modal with account creation- signal and made migrations.

--- profiles/models.py
+++ profiles/models.py
@@ -1,14 +1,9 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
-# Create your models here.
 
 class Profile(models.Model):
-    """
-    Profile model class
-    When User is created,
-    Profile is created (OneToOneField)
-    """
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -19,13 +14,15 @@ class Profile(models.Model):
     )
 
     class Meta:
-        """
-        Orders profile by created at
-        """
         ordering = ['-created_at']
 
     def __str__(self):
-        """
-        Returns profile owners profile
-        """
         return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+post_save.connect(create_profile, sender=User)
+
