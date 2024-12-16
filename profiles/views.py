a8c3c7b2d8fa672d5cc0e44abd1a929620ef2562
Brad-Hammond <brad_hammond1@sky.com>
2024-12-16 16:09:01 +0000

    Feat: Added imports to views.py.

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.db.models import Count
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from thebod_drf_api.permissions import IsOwnerOrReadOnly
+from .models import Profile
+from .serializers import ProfileSerializer
 
 # Create your views here.
