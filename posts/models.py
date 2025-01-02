9a519793f9b607f1dc484170dfc8f180761e2dbd
Brad-Hammond <brad_hammond1@sky.com>
2025-01-02 14:43:58 +0000

    Feat: Added tags_array to Post model for - categorizing posts.

--- posts/models.py
+++ posts/models.py
@@ -2,3 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    """
+    Post Model
+    """
+    tags_array = [
+        ('Puppies', 'Puppies'),
+        ('Training', 'Training'),
+        ('Health', 'Health'),
+        ('Grooming', 'Grooming'),
+        ('Adoption', 'Adoption'),
+        ('Nutrition', 'Nutrition'),
+        ('Toys', 'Toys'),
+        ('Walks', 'Walks'),
+    ]
