d85513336fcbc3f95bfd2c8a7f2bd9c9baa74bf5
Brad-Hammond <brad_hammond1@sky.com>
2025-01-02 13:45:16 +0000

    Feat: Added profile list view with comments.

--- profiles/views.py
+++ profiles/views.py
@@ -6,3 +6,41 @@ from .models import Profile
 from .serializers import ProfileSerializer
 
 # Create your views here.
+class ProfileList(generics.ListAPIView):
+    """
+ProfileList View:
+- Provides a list of profiles with annotated fields:
+  - followers_total: Total number of followers of the profile owner.
+  - following_total: Total number of users the profile owner is following.
+  - posts_total: Total number of posts created by the profile owner.
+- Allows filtering profiles based on:
+  - Profiles the owner is following.
+  - Profiles that are following the owner.
+- Supports ordering by:
+  - Total posts, followers, followings, and creation dates of relationships.
+- Utilizes Django REST Framework's generic ListAPIView.
+"""
+    queryset = Profile.objects.annotate(
+        followers_total=Count('owner__followed', distinct=True),
+        following_total=Count('owner__following', distinct=True),
+        posts_total=Count('owner__post', distinct=True)
+    ).order_by('-created_at')
+    serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+
+        # displays profile results that are following
+        'owner__following__followed__profile',
+        # displays profile results that are followed
+        'owner__followed__owner__profile',
+    ]
+    ordering_fields = [
+        'posts_total',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+        'followers_total',
+        'following_total',
+    ]
